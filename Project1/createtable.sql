54f2e5122eb07e7e2f339a2c2e9b1065624d03f9
hashiong <hongenl@uci.edu>
2020-04-09 21:03:27 -0700

    Corrected createtable.sql

--- Project1/createtable.sql
+++ Project1/createtable.sql
@@ -5,7 +5,7 @@ create table stars(id varchar(10) primary key, name varchar(100) not null, birth
 create table stars_in_movies(starId varchar(10) not null, movieId varchar(10) not null, foreign key(starID) references stars(id), foreign key(movieId) references movies(id));
 create table genres(id integer primary key AUTO_INCREMENT, name varchar(32));
 create table genres_in_movies(genreId integer not null, movieId varchar(10) not null, foreign key(genreId) references genres(id), foreign key(movieId) references movies(id));
+create table creditcards(id varchar(20) not null primary key, firstName varchar(50) not null, lastName varchar(50) not null, expiration date not null);
 create table customers(id integer not null primary key AUTO_INCREMENT, firstName varchar(50) not null, lastName varchar(50) not null, ccId varchar(20) not null, address varchar(200) not null, email varchar(50) not null, password varchar(20) not null, foreign key(ccId) references creditcards(id));
 create table sales(id integer not null primary key AUTO_INCREMENT, customerId integer not null, movieId varchar(10) not null, saleDate date not null, foreign key(customerId) references customers(id), foreign key(movieId) references movies(id));
-create table creditcards(id varchar(20) not null primary key, firstName varchar(50) not null, lastName varchar(50) not null, expiration date not null);
 create table ratings(movieId varchar(10) not null, rating float not null, numVotes integer not null, foreign key(movieId) references movies(id));
